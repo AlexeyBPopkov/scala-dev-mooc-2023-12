f5e4efa9f89164665f3d8f002f6fcc220fe8b358
Alexey Popkov <alexey.b.popkov@gmail.com>
2024-02-19 14:11:06 +0300

    Homework 7

--- src/main/scala/scala3_2/homework.scala
+++ src/main/scala/scala3_2/homework.scala
@@ -1,61 +1,71 @@
 package scala3_2
 
-
-
-
-/*
 object homework1 {
   extension (x: String)
-    def + ?????
+    def + (y: String): Int = (x + y).toInt
 
-    @main def part1Ex(): Unit ={
-      println("1" + "33")
-    }
+  @main def part1Ex(): Unit = {
+    println("56" + "3")
+  }
 }
 
 object homework2 {
   enum CompletionArg {
-    ???
-    //см приведенную ссылку
+    case ShowItIsString(s: String)
+    case ShowItIsInt(i: Int)
+    case ShowItIsFloat(f: Float)
   }
 
   object CompletionArg {
-    given fromString: Conversion[String, CompletionArg] = ???
+    given fromString: Conversion[String, CompletionArg] = ShowItIsString(_)
 
-    given fromInt: Conversion[Int, CompletionArg] = ???
+    given fromInt: Conversion[Int, CompletionArg] = ShowItIsInt(_)
 
-    given fromFloat: Conversion[Float, CompletionArg] = ???
+    given fromFloat: Conversion[Float, CompletionArg] = ShowItIsFloat(_)
   }
+
   import CompletionArg.*
 
-  @main def part2Ex(): Unit ={
+  object Completions {
+    def complete(arg: CompletionArg): String = arg match {
+      case ShowItIsString(s) => s"String: $s"
+      case ShowItIsInt(i) => s"Int: $i"
+      case ShowItIsFloat(f) => s"Float: $f"
+    }
+  }
+
+  @main def part2Ex(): Unit = {
     println(Completions.complete("String"))
     println(Completions.complete(1))
     println(Completions.complete(7f))
   }
 }
 
-
 object homework3 {
   opaque type Logarithm = Double
 
-  object Logarithm{
-    //см приведенную ссылку
+  object Logarithm {
+    def apply(d: Double): Logarithm = math.log(d)
+
+    def safeLog(d: Double): Option[Logarithm] =
+      if (d > 0) Some(math.log(d)) else None
   }
 
-  extension (x: Logarithm)
-    def toDouble: ???
-    def + (y: Logarithm): ???
-    def * (y: Logarithm): ???
+  extension (x: Logarithm) {
+    def toDouble: Double = math.exp(x)
+    def +(y: Logarithm): Logarithm = Logarithm(math.exp(x) + math.exp(y))
+    def *(y: Logarithm): Logarithm = x + y
+  }
 
-
-  @main def part3Ex(): Unit ={
-    import Logarithm
+  @main def part3Ex(): Unit = {
+    import Logarithm._
 
     val l = Logarithm(1.0)
     val l2 = Logarithm(2.0)
     val l3 = l * l2
     val l4 = l + l2
 
+    println(l3)
+    println(l4)
   }
-}*/
+}
